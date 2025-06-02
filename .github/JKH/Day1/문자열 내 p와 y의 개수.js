327c9c691999cd44a98122ffbe32ef5382a79fb2
정기현 <electrohyun@jeong-gihyeon-ui-MacBookAir.local>
2025-06-02 10:38:25 +0900

    1일차 과제

---
+++ .github/JKH/Day1/문자열 내 p와 y의 개수.js
@@ -0,0 +1,20 @@
+function solution(s){
+    
+    let numP = 0;
+    let numY = 0;
+    
+    for (i=0; i<s.length; i++) {
+        if (s[i]=='p' || s[i]=='P') {
+            numP++;
+        }
+        if (s[i]=='y' || s[i]=='Y') {
+            numY++;
+        }
+    }
+    
+    if (numP==numY || !numP && !numY) {
+        return true;
+    }
+
+    return false;
+}
---
+++ .github/JKH/Day1/서울에서 김서방 찾기.js
@@ -0,0 +1,6 @@
+function solution(seoul) {
+    const idx = seoul.indexOf('Kim');
+    
+    var answer = '김서방은 ' + idx + "에 있다";
+    return answer;
+}
