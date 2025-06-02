e6748ff8174b0100e45aa1b4d6e5a1d493254b59
YJE <c0d1ngyje@gmail.com>
2025-06-02 20:30:20 +0900

    YJE 폴더 정리

--- YJE/Day1/서울에서 김서방 찾기.js
+++
@@ -1,9 +0,0 @@
-function solution(seoul) {
-    var answer = '';
-    for(var i=0;i<seoul.length;i++){
-        if(seoul[i]==="Kim"){
-            answer=i;
-        }
-    }
-    return "김서방은 " + answer + "에 있다";
-}
