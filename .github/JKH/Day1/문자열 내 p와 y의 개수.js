0a4a9764efb41114dc86902a9b44653d16b0c312
electrohyun <dev.electrohyun@gmail.com>
2025-06-02 13:33:14 +0900

    1일차 과제

--- .github/JKH/Day1/문자열 내 p와 y의 개수.js
+++ .github/JKH/Day1/문자열 내 p와 y의 개수.js
@@ -2,16 +2,16 @@ function solution(s){
     let countP = 0;
     let countY = 0;
     
-    for (i=0; i<s.length; i++) {
-        if (s[i]=='p' || s[i]=='P') {
+    for (i = 0; i < s.length; i++) {
+        if (s[i] == 'p' || s[i] == 'P') {
             countP++;
         }
-        if (s[i]=='y' || s[i]=='Y') {
+        if (s[i] == 'y' || s[i] == 'Y') {
             countY++;
         }
     }
     
-    if (countP==countY || !countP && !countY) {
+    if (countP == countY || !countP && !countY) {
         return true;
     }
 
