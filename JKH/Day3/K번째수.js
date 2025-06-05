76328868ce262521854c59e3fe627de3e9753771
electrohyun <dev.electrohyun@gmail.com>
2025-06-05 16:04:59 +0900

    3일차 과제

--- JKH/Day3/K번째수.js
+++ JKH/Day3/K번째수.js
@@ -2,14 +2,14 @@ function solution(array, commands) {
   let answer = [];
     
   for (let i = 0; i < commands.length; i++) {
-    let startPoint = commands[i][0];
+    let startPoint = commands[i][0] - 1;
     let endPoint = commands[i][1];
-    let KPoint = commands[i][2];
+    let KPoint = commands[i][2] - 1;
 
-    let arr1 = array.slice(startPoint - 1, endPoint);
+    let arr1 = array.slice(startPoint, endPoint);
     arr1.sort((a, b) => (a - b));
           
-    answer.push(arr1[KPoint - 1]);
+    answer.push(arr1[KPoint]);
   }
     
   return answer;
