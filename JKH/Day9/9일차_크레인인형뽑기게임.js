8fc1beef0ec1f0916e9f0e5697caae286f888874
electrohyun <dev.electrohyun@gmail.com>
2025-06-20 11:17:36 +0900

    9일차 과제 수정

--- JKH/Day9/9일차_크레인인형뽑기게임.js
+++ JKH/Day9/9일차_크레인인형뽑기게임.js
@@ -10,6 +10,8 @@ function pullDoll(board, column) {
 }
 
 function addToBucket(bucket, doll) {
+  if (!doll) return 0;
+    
   if (bucket[bucket.length - 1] === doll) {
     bucket.pop();
     return 2;
@@ -30,35 +32,3 @@ function solution(board, moves) {
 
   return answer;
 }
-
-// 기존 풀이
-/*
-function solution(board, moves) {
-  let answer = 0;
-  let idx = 0;
-  const bucket = [];
-
-  for (let i = 0; i < moves.length; i++) {
-    for (let j = 0; j < board.length; j++) {
-      let target = board[j][moves[i] - 1];
-      
-      if (target) {
-        bucket.push(target);
-        board[j][moves[i] - 1] = 0;
-
-        if (bucket[idx] === bucket[idx - 1]) {
-          bucket.pop();
-          bucket.pop();
-          answer += 2;
-          idx--;
-        } else {
-          idx++;
-        }
-
-        break;
-      }
-    }
-  }
-  return answer;
-}
-*/
